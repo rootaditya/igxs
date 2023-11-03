7ff725fc362fae25bb1c0ab83b07dff1c61b4b1a
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-11-03 22:42:29 +0530

    constants

---
+++ src/constant/constant.js
@@ -0,0 +1,35 @@
+export const Name = 'Aditya';
+export const FullName = 'Aditya Kumar';
+export const SiteName = 'Incredible Graphix Studio';
+export const Email = 'contactincrediblegraphixstudio@gmail.com';
+
+export const NavList = [
+  {
+    id: 0,
+    title: 'Services',
+    url: '#services',
+  },
+  {
+    id: 1,
+    title: 'Contact Us',
+    url: '#contact',
+  },
+];
+
+export const SNSLinks = [
+  {
+    id: 0,
+    name: 'Instagram',
+    url: 'https://www.instagram.com/incrediblegraphixstudio/',
+  },
+  {
+    id: 1,
+    name: 'Youtube',
+    url: 'https://www.youtube.com/@incrediblegraphixstudio',
+  },
+  {
+    id: 2,
+    name: 'Behance',
+    url: 'https://www.behance.net/igraphixstudio',
+  },
+];
