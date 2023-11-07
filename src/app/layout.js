1b152be3aaa7a93819bcdf4d226ed873e8c5f2e8
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-11-07 16:52:33 +0530

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <body className={unrbanist.className}>
         <Header />
         {children}
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
