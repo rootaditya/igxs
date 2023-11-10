6bd0826eb31ffa1148878d8da09c892e3695886a
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-11-10 16:53:26 +0530

    assets index

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,21 @@
 import MainLogo from './logo.svg';
 import HomeGradient from './homeGradient.svg';
+import AssertionImage from './assertion.svg';
+import StarIcon from './star.svg';
+import Star2Icon from './star2.svg';
+import QuoteIcon from './quote.svg';
+import EcclipseBG from './ecclipse.svg';
+import ArrowIcon from './arrowIcon.svg';
+import EmailIcon from './email.svg';
 
-export { MainLogo, HomeGradient };
+export {
+  MainLogo,
+  HomeGradient,
+  AssertionImage,
+  StarIcon,
+  Star2Icon,
+  QuoteIcon,
+  EcclipseBG,
+  ArrowIcon,
+  EmailIcon,
+};
