b9d18dccae451230e59650e444e03342c83ba640
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-12-03 14:28:35 +0530

    SEO Meta Data

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,9 +6,22 @@ import Footer from '@/components/Footer';
 const unrbanist = Urbanist({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'IGXS - Your X-desires destiny',
-  description:
-    'fullfill your x-desires here watch all porn video for free without any interuption or ads',
+  title: 'IGXS - Graphic and UI/UX Design Services',
+  description: 'Get the Certified and Premium looking design',
+  tag: [
+    'IGXS',
+    'Incredible Graphix Studio',
+    'Incrediblegraphixstudio',
+    'Aditya Kumar',
+    'UI Design',
+    'UX Design',
+    'UI/UX',
+    'Graphic Design',
+    'Figma',
+    'Photoshop',
+    'Adobe',
+    'UI/UX Designer',
+  ],
 };
 
 export default function RootLayout({ children }) {
