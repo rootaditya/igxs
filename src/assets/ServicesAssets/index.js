8e933c2ea8cee219592f768a618e00465f321933
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-12-04 14:29:01 +0530

    Services Images

--- src/assets/ServicesAssets/index.js
+++ src/assets/ServicesAssets/index.js
@@ -1,3 +1,25 @@
 import UIUXCardImg from './UIUX.svg';
+import PosterCardImg from './Poster.svg';
+import LogoCardImg from './Logo.svg';
+import SocialCardImg from './Social.svg';
+import PosterDesignCardImg from './PosterDesign.svg';
+import BrandCardImg from './Brand.svg';
+import PrintCardImg from './Print.svg';
+import Print2CardImg from './Print2.svg';
+import Print3CardImg from './Print3.svg';
+import StanddeeCardImg from './Standee.svg';
+import CarCardImg from './Car.svg';
 
-export { UIUXCardImg };
+export {
+  UIUXCardImg,
+  PosterCardImg,
+  LogoCardImg,
+  SocialCardImg,
+  PosterDesignCardImg,
+  BrandCardImg,
+  PrintCardImg,
+  Print2CardImg,
+  Print3CardImg,
+  StanddeeCardImg,
+  CarCardImg,
+};
