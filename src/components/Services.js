12ef527bdcc4ded48944171c07b9593c3fb9feaa
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-12-20 14:34:11 +0530

    components

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,8 +6,78 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import {
+  BrandCardImg,
+  CarCardImg,
+  LogoCardImg,
+  PosterDesignCardImg,
+  Print2CardImg,
+  Print3CardImg,
+  PrintCardImg,
+  SocialCardImg,
+  StanddeeCardImg,
+  UIUXCardImg,
+  PosterCardImg,
+} from '@/assets/ServicesAssets';
 
 export default function Services() {
+  const carouselData = [
+    {
+      id: 0,
+      title: 'UI/UX Design',
+      image: UIUXCardImg,
+    },
+    {
+      id: 1,
+      title: 'Logo Design',
+      image: LogoCardImg,
+    },
+    {
+      id: 2,
+      title: 'Social Media Design',
+      image: SocialCardImg,
+    },
+    {
+      id: 3,
+      title: 'Poster Design',
+      image: PosterDesignCardImg,
+    },
+    {
+      id: 4,
+      title: 'Brand Design',
+      image: BrandCardImg,
+    },
+    {
+      id: 5,
+      title: 'Print Design',
+      image: PrintCardImg,
+    },
+    {
+      id: 6,
+      title: 'Print 2 Design',
+      image: Print2CardImg,
+    },
+    {
+      id: 7,
+      title: 'Print 3 Design',
+      image: Print3CardImg,
+    },
+    {
+      id: 8,
+      title: 'Standee Design',
+      image: StanddeeCardImg,
+    },
+    {
+      id: 9,
+      title: 'Car Livery Design',
+      image: CarCardImg,
+    },
+    {
+      id: 10,
+      title: 'Poster Design',
+      image: PosterCardImg,
+    },
+  ];
   return (
     <section id="services" className="w-full relative rounded-t-xl mt-5">
       <div className="bg-services absolute left-0 top-0 h-full w-full rounded-t-xl z-0"></div>
@@ -43,24 +113,11 @@ export default function Services() {
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log('slide change')}
       >
-        <SwiperSlide>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Card />
-        </SwiperSlide>
+        {carouselData.map((item, id) => (
+          <SwiperSlide key={id}>
+            <Card title={item.title} image={item.image} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
